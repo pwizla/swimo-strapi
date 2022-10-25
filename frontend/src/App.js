79e0ad1003d657e50cc219bd1b2400f1d80ed400
Pierre Wizla <pwizla+github@gmail.com>
2022-10-25 15:58:36 +0200

    Move operations to its own component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,7 @@ import{
   Route,
   Link,
 } from "react-router-dom";
-import {
-  useEffect,
-  useState
-} from 'react'
-// import Operations from "./operations";
+import Operations from "./Operations";
 import './App.css';
 
 function App() {
@@ -30,64 +26,6 @@ function Home() {
       Welcome to Swimo!
       <Link className="router-link" to="/operations">Voir opérations</Link>
     </header>
-  )
-}
-
-
-function Operations() {
-  const [error, setError] = useState(null);
-  const [operations, setOperations] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:1337/api/operations', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then( response => response.json())
-      .then( ({ data }) => { 
-        setOperations(data)
-      })
-      .catch((error) => setError(error))
-  }, [])
-
-  if (error) {
-    return <div>An error occured: {error.message}</div>;
-  }
-
-  return (
-    <div className="operations">
-      <h1>Liste d'opérations</h1>
-      { operations
-        ? <table
-            cellPadding="10px"
-            rules="groups"
-            className="operations-table">
-            <thead>
-              <tr>
-                <th>Libellé</th>
-                <th>Date</th>
-                <th>Montant</th>
-                <th>Banque</th>
-              </tr>
-            </thead>
-            <tbody>
-              {operations.map(({ id, attributes }) => (
-                <tr key={id}>
-                  <td>{attributes.Libelle}</td>
-                  <td>{attributes.Date}</td>
-                  <td className={`number ${attributes.Montant > 0 ? "positive" : "negative"}`}>
-                    {attributes.Montant}
-                  </td>
-                  <td>{attributes.Check ? '✅' : ''}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        : <div>Nous n'avons malheureusement pas pu retrouver la liste d'opérations… 😅</div>
-      }
-    </div>
   )
 }
 
---
+++ frontend/src/Operations.js
@@ -0,0 +1,64 @@
+import React from "react";
+import {
+  useEffect,
+  useState
+} from 'react'
+
+function Operations() {
+  const [error, setError] = useState(null);
+  const [operations, setOperations] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:1337/api/operations', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then( response => response.json())
+      .then( ({ data }) => { 
+        setOperations(data)
+      })
+      .catch((error) => setError(error))
+  }, [])
+
+  if (error) {
+    return <div>An error occured: {error.message}</div>;
+  }
+
+  return (
+    <div className="operations">
+      <h1>Liste d'opérations</h1>
+      { operations
+        ? <table
+            cellPadding="10px"
+            rules="groups"
+            className="operations-table">
+            <thead>
+              <tr>
+                <th>Libellé</th>
+                <th>Date</th>
+                <th>Montant</th>
+                <th>Banque</th>
+              </tr>
+            </thead>
+            <tbody>
+              {operations.map(({ id, attributes }) => (
+                <tr key={id}>
+                  <td>{attributes.Libelle}</td>
+                  <td>{attributes.Date}</td>
+                  <td className={`number ${attributes.Montant > 0 ? "positive" : "negative"}`}>
+                    {attributes.Montant}
+                  </td>
+                  <td>{attributes.Check ? '✅' : ''}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        : <div>Nous n'avons malheureusement pas pu retrouver la liste d'opérations… 😅</div>
+      }
+    </div>
+  )
+}
+
+export default Operations;
