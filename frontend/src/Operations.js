6c3d47cd8e6b588688ed3061fceece5ccdf04950
Pierre Wizla <pwizla+github@gmail.com>
2022-11-18 21:38:56 +0100

    Add first version of months switcher

--- frontend/src/Operations.js
+++ frontend/src/Operations.js
@@ -6,49 +6,121 @@ import {
 // import MonthPicker from './MonthPicker'
 const qs = require('qs');
 
-function getQuery() {
-
-  const {firstDayOfMonth, lastDayOfMonth} = getCurrentMonth();
-
-  return qs.stringify(
-    {
-      filters: {
-        'Date': {
-          $between: [firstDayOfMonth, lastDayOfMonth]
-        }
-      },
-      sort: ['Date:asc']
-    },
-    {
-      encodeValuesOnly: true
-    }
-  )
-}
-
-function getCurrentMonth() {
-  const today = new Date()
-  const month = today.getMonth() + 1
-  const year = today.getFullYear()
-  const firstDayOfMonth = [year, month, '01'].join('-')
-  const lastDayOfMonth = [year, month, '30'].join('-') // should adapt to get last day of month
-
-  const locale = "fr-fr";
-  const monthName = today.toLocaleString(locale, { month: "long" });
-
-  return {
-    firstDayOfMonth,
-    lastDayOfMonth,
-    monthName,
-    year
+function matchMonthNames(monthNumber){
+  let monthName, numberOfDaysPerMonth;
+  switch(monthNumber) {
+    case 1:
+      monthName = 'janvier';
+      numberOfDaysPerMonth = 31;
+      break;
+    case 2:
+      monthName = 'février';
+      numberOfDaysPerMonth = 28;
+      break;
+    case 3:
+      monthName = 'mars';
+      numberOfDaysPerMonth = 31;
+      break;
+    case 4:
+      monthName = 'avril';
+      numberOfDaysPerMonth = 30;
+      break;
+    case 5:
+      monthName = 'mai';
+      numberOfDaysPerMonth = 31;
+      break;
+    case 6:
+      monthName = 'juin';
+      numberOfDaysPerMonth = 30;
+      break;
+    case 7:
+      monthName = 'juillet';
+      numberOfDaysPerMonth = 31;
+      break;
+    case 8:
+      monthName = 'août';
+      numberOfDaysPerMonth = 31;
+      break;
+    case 9:
+      monthName = 'septembre';
+      numberOfDaysPerMonth = 30;
+      break;
+    case 10:
+      monthName = 'octobre';
+      numberOfDaysPerMonth = 31;
+      break;
+    case 11:
+      monthName = 'novembre';
+      numberOfDaysPerMonth = 30;
+      break;
+    case 12:
+      monthName = 'décembre';
+      break;
+    default: 
+      monthName = new Date().toLocaleString('fr-fr', { month: "long" }); 
   }
+  return monthName;
 }
 
 function Operations() {
   const [error, setError] = useState(null);
   const [operations, setOperations] = useState([]);
+  let [displayedMonthNumber, setDisplayedMonthNumber] = useState(new Date().getMonth() + 1);
+  let [displayedYear, setDisplayedYear] = useState(new Date().getFullYear());
+  let [displayedMonthName, setDisplayedMonthName] = useState(new Date().toLocaleString('fr-fr', { month: "long" }));
+  const [query, setQuery] = useState(getQuery(new Date().getMonth() + 1));
+
+  function addMonth() {
+    if (displayedMonthNumber === 12) { 
+      displayedMonthNumber = 0;
+      displayedMonthName = 'janvier';
+      setDisplayedYear(displayedYear + 1);
+    }
+    setDisplayedMonthNumber(displayedMonthNumber + 1);
+    setDisplayedMonthName(matchMonthNames(displayedMonthNumber + 1));
+    setQuery(getQuery(displayedMonthNumber + 1));
+    console.log(displayedMonthNumber, displayedMonthName);
+  }
+
+  function substractMonth() {
+    if (displayedMonthNumber < 1) { 
+      displayedMonthNumber = 12;
+      displayedMonthName = 'décembre';
+      setDisplayedYear(displayedYear - 1);
+    }
+    setDisplayedMonthNumber(displayedMonthNumber - 1);
+    setDisplayedMonthName(matchMonthNames(displayedMonthNumber - 1));
+    setQuery(getQuery(displayedMonthNumber - 1));
+    console.log(displayedMonthNumber, displayedMonthName);
+  }
+
+  function formatMonthNumber(monthNumber) {
+    const formatted = monthNumber < 10 ? `0${monthNumber.toString()}` : `${monthNumber.toString()}`
+    return formatted
+  }
+
+  function getQuery(displayedMonthNumber, numberOfDaysPerMonth) {
+
+    const firstDayOfMonth = `${displayedYear}-${formatMonthNumber(displayedMonthNumber)}-01`;
+    const lastDayOfMonth = `${displayedYear}-${formatMonthNumber(displayedMonthNumber)}-30`;
+  
+    return qs.stringify(
+      {
+        filters: {
+          'Date': {
+            $between: [firstDayOfMonth, lastDayOfMonth]
+          }
+        },
+        sort: ['Date:asc']
+      },
+      {
+        encodeValuesOnly: true
+      }
+    )
+  }
 
   useEffect(() => {
-    fetch(`http://localhost:1337/api/operations?${getQuery()}`, {
+    fetch(`http://localhost:1337/api/operations?${query}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -59,19 +131,21 @@ function Operations() {
         setOperations(data)
       })
       .catch((error) => setError(error))
-  }, [])
+  }, [query])
 
   if (error) {
     return <div>An error occured: {error.message}</div>;
   }
   
-  const currentMonthName = `${getCurrentMonth().monthName} ${getCurrentMonth().year}`;
+  const currentMonthName = `${displayedMonthName} ${displayedYear}`;
 
   // TODO: add prev/next month selector
   return (
     <div className="operations">
+      <button onClick={substractMonth}>Mois précédent</button>
+      <button onClick={addMonth}>Mois suivant</button>
       <h1>Liste d'opérations pour le mois de <span className="current current-month">{currentMonthName}</span></h1>
-      { operations.length > 0
+      { operations && operations.length > 0
         ? <table
             cellPadding="10px"
             rules="groups"
